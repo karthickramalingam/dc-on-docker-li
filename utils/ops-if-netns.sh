2d492b66f43e3738390c11537d21f3fa6a58ff3a
karthickramalingam <karthickramalingam@github.com>
2016-07-28 22:14:02 +0530

    Modified for the Latest Image - Rel Dill

--- utils/ops-if-netns.sh
+++ utils/ops-if-netns.sh
@@ -5,6 +5,7 @@ count=0
 for i in {1..48}; do
 	ip link set eth${i} > /dev/null
 	if [ $? = 0 ]; then
+		ip netns exec swns ip link delete ${i}
 		ip link set eth${i} down
 		ip link set eth${i} name ${i}
 		ip link set ${i} netns swns
